1e99cb38446e534ed26ecce5c2e12417dcb39601
mbronek7 <mbronek7@gmail.com>
2018-10-16 17:36:55 +0200

    App Basic Configuration (#1)

---
+++ app/javascript/packs/hello_world/hello_world.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: I18n.t('hello')
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 4em;
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
